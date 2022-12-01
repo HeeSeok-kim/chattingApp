fc40044333ec870f9abd18f3e2d485eb9ec98e8f
kimheeseok <kim1140129@gmail.com>
2022-12-01 23:45:52 +0900

    노드 패키지 추가

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config';
-import express,{ Request, Response } from "express";
+import express, {NextFunction, Request, Response} from "express";
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+
+
+app.use((error:Error,req:Request, res:Response, next:NextFunction) => {
+    next();
+});
 
 app.listen(PORT, () => {
     console.log(PORT, "서버가 실행되었습니다.");
